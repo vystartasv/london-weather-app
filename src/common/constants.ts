53bc33010582decf9507662b360d7a3823c304e0
Vilius Vystartas <vilius.vystartas@gmail.com>
2020-09-13 11:42:16 +0100

    Update constants.ts

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
   public static LONDON: { lat: number, lon: number } = { lat: 51.51, lon: -0.13 };
-  public static API_KEY: string = "3c83498f7e2deb8458f2e9ec8b2c1b23"; // Actual key of mine, please use responsibly 
+  public static API_KEY: string = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; // Actual key of mine, please use responsibly 
   public static WEEKDAYS: string[] = [
     "SUN",
     "MON",
